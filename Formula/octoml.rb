6df2250960ac305a8c134a7c2c59d813bc5150ca
Jared Roesch <jroesch@octoml.ai>
2022-06-16 17:56:35 -0700

    Add Formula for OctoML CLI version 0.4.4

---
+++ Formula/octoml.rb
@@ -0,0 +1,17 @@
+class OctoML < Formula
+  desc "The OctoML CLI for packaging and deploying models."
+  homepage "https://octoml.ai"
+  url "https://downloads.octoml.ai/octoml_macOS_v0.4.4.zip"
+  sha256 "86846d8c2d175808453d6923d2c2e8ecdc6ce675aa353bb33cc2528cd0e64b5a"
+  license ""
+
+  conflicts_with "octoml"
+
+  def install
+    bin.install "octoml"
+  end
+
+  test do
+    system "#{bin}/octoml --version"
+  end
+end
